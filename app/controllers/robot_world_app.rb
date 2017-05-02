a6ea1f4eeade345fb5ec2e156fc7c4a7276dfa92
Thomas Soucie <tsoucie1858@gmail.com>
2017-05-02 15:50:01 -0600

    Initial commit

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,16 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    erb :index
+  end
+
   get '/robots' do
     erb :index
   end
+
+  get'/robots/new' do
+    erb :new
+  end 
 end
 
