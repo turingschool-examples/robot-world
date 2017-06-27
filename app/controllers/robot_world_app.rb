a961fdd539a91d37fb6143f5f76b2861007b626f
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-06-27 17:33:15 -0600

    main CRUD piece finished

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -2,9 +2,42 @@ require_relative '../models/robot.rb'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
-end
+  # get '/robots' do
+  #   erb :index
+  # end
+  get '/robots/new' do
+    erb :new
+  end
 
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
+  end
+
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+
+  get '/robots/:id/edit' do
+      @robot = Robot.find(params[:id])
+      erb :edit
+  end
+
+  put '/robots/:id' do |id|
+    Robot.update(id.to_i, params[:robot])
+    redirect "/robots/#{id}"
+  end
+
+  delete '/robots/:id' do |id|
+    Robot.destroy(id.to_i)
+    redirect '/robots'
+  end
+end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,10 +1,58 @@
 require 'sqlite3'
 
 class Robot
-  attr_reader :id, :name, :city, :state, :department 
+  attr_reader :id, :name, :city, :state, :department
 
   def initialize(robot_params)
-    @database = SQLite3::Database.new('db/robot_world_development.db')
+    @id         = robot_params["id"] if robot_params["id"]
+    @name       = robot_params["name"]
+    @city       = robot_params["city"]
+    @state      = robot_params["state"]
+    @department = robot_params["department"]
+    @database   = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
   end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department) VALUES (?, ?, ?, ?);", @name, @city, @state, @department)
+  end
+
+  def self.find(id)
+    robot = database.execute("SELECT * FROM robots WHERE id = ?", id).first
+    Robot.new(robot)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/robot_world_development.db')
+    database.results_as_hash = true
+    database
+  end
+
+  def self.all
+    robots = database.execute("SELECT * FROM robots")
+    robots.map do |robot|
+      Robot.new(robot)
+    end
+  end
+
+  def self.update(id, robot_params)
+    database.execute("UPDATE robots
+                      SET name = ?,
+                      city = ?,
+                      state = ?,
+                      department = ?
+                      WHERE id = ?;",
+                      robot_params[:name],
+                      robot_params[:city],
+                      robot_params[:state],
+                      robot_params[:department],
+                      id)
+    Robot.find(id)
+  end
+
+  def self.destroy(id)
+    database.execute("DELETE FROM robots
+                      WHERE id = ?;", id)
+  end
+  
 end
