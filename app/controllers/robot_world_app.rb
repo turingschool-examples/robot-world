3791bceaf8eb2aeb3847c427262d7ad77f192f68
jtruong2 <jtruong2@comcast.net>
2017-06-27 14:52:17 -0600

    create new robots

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,21 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  # get '/' do
+  #   erb :layout
+  # end
+
   get '/robots' do
     erb :index
   end
-end
 
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,18 @@
 require 'sqlite3'
 
 class Robot
+  attr_reader :id, :name, :city, :state, :department
   def initialize(robot_params)
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
+    @id = robot_params[:id] if robot_params[:id]
+    @name = robot_params[:name]
+    @city = robot_params[:city]
+    @state = robot_params[:state]
+    @department = robot_params[:department]
+  end
+
+  def self.all
+
   end
 end
