a55dd038ca3f075c26a20aa3977832a5175a86e8
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-06-27 14:42:11 -0600

    initial commit

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,6 +1,8 @@
 require 'sqlite3'
 
 class Robot
+  attr_reader :id, :name, :city, :state, :department 
+
   def initialize(robot_params)
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
