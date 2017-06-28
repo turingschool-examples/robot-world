4c138ea717b30e811dd06c7cc4673f3c7d3adb53
jtruong2 <jtruong2@comcast.net>
2017-06-27 18:06:29 -0600

    finish robots CRUD

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -2,10 +2,11 @@ require_relative '../models/robot.rb'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
-  # get '/' do
-  #   erb :layout
-  # end
+  get '/' do
+    erb :dashboard
+  end
 
   get '/robots' do
     @robots = Robot.all
@@ -17,9 +18,28 @@ class RobotWorldApp < Sinatra::Base
   end
 
   post '/robots' do
-    @robot = Robot.new(params[:robot])
-    @robot.save
-    require 'pry'; binding pry
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+
+  get '/robots/:id' do
+    @robot = Robot.find(params[:id])
+    erb :show
+  end
+
+  get '/robots/:id/edit' do
+    @robot = Robot.find(params[:id])
+    erb :edit
+  end
+
+  put '/robots/:id' do |id|
+    Robot.update(id.to_i, params[:robot])
+    redirect "/robots/#{id}"
+  end
+
+  delete '/robots/:id' do |id|
+    Robot.destroy(id.to_i)
     redirect '/robots'
   end
 end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -24,6 +24,20 @@ class Robot
     end
   end
 
+  def self.find(id)
+    robot = database.execute("SELECT * FROM robots WHERE id = ?", id).first
+    Robot.new(robot)
+  end
+
+  def self.update(id, robot_params)
+    robot = database.execute("UPDATE robots SET name = ?, city = ?, state = ?, department = ? WHERE id = ?", robot_params[:name], robot_params[:city], robot_params[:state], robot_params[:department], id)
+    Robot.find(id)
+  end
+
+  def self.destroy(id)
+    database.execute("DELETE FROM robots WHERE id = ?", id)
+  end
+
   def self.database
     database = SQLite3::Database.new("db/robot_world_development.db")
     database.results_as_hash = true
