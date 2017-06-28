5f7a6300815d9850eda0da2f0238b047b7d0823e
Daniel Phillips <danieledward.phillips@gmail.com>
2017-06-27 18:21:20 -0600

    adds update

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,13 +3,34 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    redirect '/robots'
+  end
+
   get '/robots' do
     @robot = Robot.all
     erb :index
+  end
+
+  get '/robots/new_robot' do
+    erb :new_robot
   end
 
   get '/robots/:id' do |id|
     @robot = Robot.find(id)
     erb :show
   end
+
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+
+  get '/robots/:id/edit' do
+    @robot = Robot.find(params[:id])
+    erb :edit
+  end
+
+
 end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,13 +1,16 @@
 require 'sqlite3'
 
 class Robot
-  attr_reader :name, :id
+  attr_reader :name, :id, :city, :state, :department
 
   def initialize(robot_params)
     @name = robot_params["name"]
+    @city = robot_params["city"]
+    @state  = robot_params["state"]
+    @department = robot_params["department"]
+    @id = robot_params["id"] if robot_params["id"]
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
-    @id = robot_params["id"] if robot_params["id"]
   end
 
   def self.all
@@ -15,6 +18,15 @@ class Robot
     robot.map do |task|
       Robot.new(task)
     end
+  end
+
+  def self.find(id)
+    robot = database.execute("SELECT * FROM robots WHERE id = ?", id).first
+    Robot.new(robot)
+  end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department) VALUES (?, ?, ?, ?);", @name, @city, @state, @department)
   end
 
   def self.database
