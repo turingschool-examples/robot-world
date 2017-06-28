3bbcb375fae11d826903900f1f750f9d15831ed6
jtruong2 <jtruong2@comcast.net>
2017-06-28 08:41:52 -0600

    fix display issue

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -4,9 +4,9 @@ class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
   set :method_override, true
 
-  get '/' do
-    erb :dashboard
-  end
+  # get '/' do
+  #   erb :dashboard
+  # end
 
   get '/robots' do
     @robots = Robot.all
