a8e25f3375ef9dc0fd5469d23266777f05134d4f
Yohanan Assefa <nanahoy@gmail.com>
2017-06-27 23:15:36 -0600

    Added functionality and some style

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,57 @@
 require 'sqlite3'
 
 class Robot
+  attr_reader :name, :city, :state, :department, :id
+
   def initialize(robot_params)
+    @name       = robot_params["name"]
+    @city       = robot_params["city"]
+    @state      = robot_params["state"]
+    @department = robot_params["department"]
+    @id         = robot_params["id"] if robot_params["id"]
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
   end
+
+  def self.database
+    database = SQLite3::Database.new('db/robot_world_development.db')
+    database.results_as_hash = true
+    database
+  end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department)
+                      VALUES (?, ?, ?, ?);", @name, @city, @state, @department)
+  end
+
+  def self.all
+    robots = database.execute("SELECT * FROM robots")
+    robots.map do |robot|
+      Robot.new(robot)
+    end
+  end
+
+  def self.find(id)
+    robot = database.execute("SELECT * FROM robots WHERE id = ?", id).first
+    Robot.new(robot)
+  end
+
+  def self.update(id, robot_params)
+    database.execute("UPDATE robots SET name = ?,
+                                        city = ?,
+                                        state = ?,
+                                        department = ?
+                                        WHERE id = ?;",
+                                        robot_params[:name],
+                                        robot_params[:city],
+                                        robot_params[:state],
+                                        robot_params[:department],
+                                        id)
+  end
+
+  def self.destroy(id)
+    database.execute("DELETE FROM robots WHERE id = ?;", id)
+  end
+
+
 end
